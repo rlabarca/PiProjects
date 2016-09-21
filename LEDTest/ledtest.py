2061a5a31f34765b4412724ac6f6deb6d1f601a8
Rich LaBarca <rich@labarca.org>
2016-09-21 07:08:46 +0500

    Blinking test working

--- LEDTest/ledtest.py
+++ LEDTest/ledtest.py
@@ -1,5 +1,17 @@
 import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH)
+import time
+# blinking function
+def blink(pin):
+        GPIO.output(pin,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(pin,GPIO.LOW)
+        time.sleep(1)
+        return
+# to use Broadcomm pin numbers
+GPIO.setmode(GPIO.BCM)
+# set up GPIO output channel
+GPIO.setup(21, GPIO.OUT)
+# blink GPIO17 50 times
+for i in range(0,5):
+        blink(21)
+GPIO.cleanup() 
