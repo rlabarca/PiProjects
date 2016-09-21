e87c3d93b93471cbd0cda0508c49bb3bf7c46863
Rich LaBarca <rich@labarca.org>
2016-09-21 16:40:42 +0500

    Works. Polling changed to edge callbacks. SIGNINT exit handler cleanup.

--- HighFive/highfive.py
+++ HighFive/highfive.py
@@ -1,18 +1,27 @@
 import RPi.GPIO as GPIO
 import time
+import signal
+import sys
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
-GPIO.setup(3, GPIO.OUT)         #LED output pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20, GPIO.IN)                     #Read signal from PIR motion sensor
+GPIO.setup(21, GPIO.OUT, initial=0)         #LED output pin
+
+def signal_handler(signal, frame):
+        print('EXIT: Cleaning Up')
+        GPIO.cleanup()
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 while True:
-       i=GPIO.input(11)
-       if i==0:                 #When output from motion sensor is LOW
-             print "No intruders",i
-             GPIO.output(3, 0)  #Turn OFF LED
-             time.sleep(0.1)
-       elif i==1:               #When output from motion sensor is HIGH
-             print "Intruder detected",i
-             GPIO.output(3, 1)  #Turn ON LED
-             time.sleep(0.1)
+	
+	GPIO.wait_for_edge(20, GPIO.RISING)
+	print "High Five!"
+	GPIO.output(21, 1) #Turn on LED
+
+	GPIO.wait_for_edge(20, GPIO.FALLING)
+	print "Recoil"
+	GPIO.output(21, 0) #Turn off LED
+
+
