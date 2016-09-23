aa1ae978a732f757a0a1ba141be4fb96d5af34d4
Rich LaBarca <rich@labarca.org>
2016-09-22 21:27:53 -0400

    Initial servo code - untested.

--- HighFive/highfive.py
+++ HighFive/highfive.py
@@ -7,17 +7,24 @@ import sys
 # All IO specified in BCM pinout
 INPUT_MOTION = 20
 OUTPUT_LED = 21
+OUTPUT_SERVO = 12
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(20, GPIO.IN)                     #Read signal from PIR motion sensor
-GPIO.setup(21, GPIO.OUT, initial=0)         #LED output pin
+GPIO.setup(INPUT_MOTION, GPIO.IN)                     #Read signal from PIR motion sensor
+GPIO.setup(OUTPUT_LED, GPIO.OUT, initial=0)         #LED output pin
+GPIO.setup(OUTPUT_SERVO, GPIO.OUT)						#Servo output pin
 
 def signal_handler(signal, frame):
         print('EXIT: Cleaning Up')
         GPIO.cleanup()
         sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
+
+
+pwm=GPIO.PWM(OUTPUT_SERVO,50)
+pwm.start(5)
+pwm.ChangeDutyCycle(7.5)
 
 while True:
 	
