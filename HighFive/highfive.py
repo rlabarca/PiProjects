e00ed75449be37a9fae3cea0cf74402b9b12af5b
Rich LaBarca <rich@labarca.org>
2016-09-23 22:36:35 +0500

    Version 1 with servo being controlled by motion sensor. Proper cleanup / reset of servo and GPIO

--- HighFive/highfive.py
+++ HighFive/highfive.py
@@ -9,6 +9,10 @@ INPUT_MOTION = 20
 OUTPUT_LED = 21
 OUTPUT_SERVO = 12
 
+#Servo angles
+SERVO_DC_MIN = 2
+SERVO_DC_MAX = 10
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(INPUT_MOTION, GPIO.IN)                     #Read signal from PIR motion sensor
@@ -17,23 +21,27 @@ GPIO.setup(OUTPUT_SERVO, GPIO.OUT)						#Servo output pin
 
 def signal_handler(signal, frame):
         print('EXIT: Cleaning Up')
+	pwm.ChangeDutyCycle(SERVO_DC_MIN)
+	time.sleep(1);
         GPIO.cleanup()
         sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
 
 pwm=GPIO.PWM(OUTPUT_SERVO,50)
-pwm.start(5)
-pwm.ChangeDutyCycle(7.5)
+pwm.start(SERVO_DC_MIN)
+
 
 while True:
+
 	
 	GPIO.wait_for_edge(INPUT_MOTION, GPIO.RISING)
 	print "High Five!"
+	pwm.ChangeDutyCycle(SERVO_DC_MAX)
 	GPIO.output(OUTPUT_LED, 1) #Turn on LED
 
 	GPIO.wait_for_edge(INPUT_MOTION, GPIO.FALLING)
+	pwm.ChangeDutyCycle(SERVO_DC_MIN)
 	print "Recoil"
 	GPIO.output(OUTPUT_LED, 0) #Turn off LED
-
 
