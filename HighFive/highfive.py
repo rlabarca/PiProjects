1097a8f03384a6067c1b760e02655aedfaf520a3
Rich LaBarca <rich@labarca.org>
2016-09-22 21:06:07 -0400

    Minor cleanup. Untested.

--- HighFive/highfive.py
+++ HighFive/highfive.py
@@ -3,6 +3,11 @@ import time
 import signal
 import sys
 
+
+# All IO specified in BCM pinout
+INPUT_MOTION = 20
+OUTPUT_LED = 21
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20, GPIO.IN)                     #Read signal from PIR motion sensor
@@ -16,12 +21,12 @@ signal.signal(signal.SIGINT, signal_handler)
 
 while True:
 	
-	GPIO.wait_for_edge(20, GPIO.RISING)
+	GPIO.wait_for_edge(INPUT_MOTION, GPIO.RISING)
 	print "High Five!"
-	GPIO.output(21, 1) #Turn on LED
+	GPIO.output(OUTPUT_LED, 1) #Turn on LED
 
-	GPIO.wait_for_edge(20, GPIO.FALLING)
+	GPIO.wait_for_edge(INPUT_MOTION, GPIO.FALLING)
 	print "Recoil"
-	GPIO.output(21, 0) #Turn off LED
+	GPIO.output(OUTPUT_LED, 0) #Turn off LED
 
 
